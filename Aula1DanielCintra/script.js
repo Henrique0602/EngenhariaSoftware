6808bcc59942b364edcaf07f44575c7c83907a49
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 20:10:40 -0300

    Aulinha

---
+++ Aula1DanielCintra/script.js
@@ -0,0 +1,123 @@
+console.log("Hello World")
+
+// operador
+
+//let idade = 4
+// let resultado = idade > 18 ?"Maior de idade" : "Menor de idade"
+
+//console.log("resultado")
+
+//Coalesncia nula
+
+//let valor = 10 
+//let resultado = valor ?? "Valor padrão"
+//console.log(resultado)
+
+
+//Encadamento Opcional
+
+//let usuario = {nome:"Ana", idade:10}
+//console.log(usuario?.idade?.rua)
+
+
+//Arrays
+
+//Operador Spread
+
+//let arrl = [1,2]
+//let arrl2 = [1,2]
+//console.log(arrl)
+//console.log(arrl2)
+
+//let combinando = [...arrl,...arrl2]
+
+//console.log(combinando)
+
+//Javascript1 ACABOU AQUI 
+
+//JavaScript2 Começa aqui 
+
+//If ou Else
+
+//let tarefaconcluida = false 
+
+//if(tarefaconcluida){
+   // console.log("A tarefa esta concluida")
+//}else{
+    //console.log("A tarefa não esta concluida")
+//}
+
+//let prioridade = 2
+//if(prioridade===1){
+    //console.log("Prioridade baixa")
+//}else if(prioridade === 2){
+   //console.log("prioridade média")
+//}else if(prioridade === 3){
+    //console.log("prioridade alta")
+//}else{
+    //console.log("Prioridade desconhecida")
+//}
+
+//Switch case
+
+//let diaSemana = new Date("04-01-2025").getDay()
+
+//console.log(diaSemana)
+
+//switch(diaSemana){
+    //case 0:
+    //console.log("Domingo")
+   // break
+
+    //case 1:
+    //console.log("Segunda")
+    //break
+
+    //case 2:
+    //console.log("Terça")
+    //break
+
+    //case 3:
+    //console.log("Quarta")
+    //break
+
+    //case 4:
+    //console.log("Quinta")
+    //break
+
+    //case 5:
+    //console.log("Sexta")
+    //break
+
+    //case 6:
+    //console.log("Sabado")
+   // break
+
+   // default:
+   // console.log("Dia Inválido")
+   // break
+
+//}
+
+
+//Loops
+
+//For
+
+//for (let i=0; i > -10; i-- ){
+    ///console.log("Contagem:",i)
+//}
+
+//while
+
+//let i = 0
+//while(i <5){
+    //console.log("Contagem: ", i)
+    //i++
+//}
+
+//let i = 0;
+//do{
+   // console.log("Contagem: ", i)
+    //i++
+//}while(i <5)
